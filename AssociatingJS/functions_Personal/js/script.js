04b7a2b02815b128996cb0a6d4878a869ed74bce
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-26 13:03:02 -0400

    functions_Personal: set up formula, input first set of prompts

--- AssociatingJS/functions_Personal/js/script.js
+++ AssociatingJS/functions_Personal/js/script.js
@@ -4,4 +4,21 @@
  */
 
 
-alert("Test2 for personal ");
+alert("I have a lot of leaves to rake up and put in trash bags. \n\nI will pay some neighborhood kids a fixed amount per bag. Have to see how much it will cost me. ")
+
+
+var lukeBags =prompt("How many trash bags did Luke fill up?")
+
+var jacobBags =prompt ("How many trash bags did Jacob fill up")
+
+
+
+
+
+
+console.log(Number(lukeBags)+Number(jacobBags));  //a test prompt
+
+
+
+
+//alert("Test2 for personal ");
--- AssociatingJS/functions_Wacky/js/script.js
+++ AssociatingJS/functions_Wacky/js/script.js
@@ -4,4 +4,22 @@
  */
 
 
-alert("Test3 for fucntions wacky wacky");
+
+
+
+
+/** function dogYears(age, currentAge){
+    var dogYears = age*currentAge* 78;
+    console.log("So my dogs age is " + dogYears);
+}
+
+
+                                                                        ///Used as a tool to figure out Functions, will delete
+dogYears(4,7);
+dogYears(7,7);
+
+
+
+//alert("Test3 for fucntions wacky wacky");
+
+ */

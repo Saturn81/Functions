2357bbd10dcb0e0e9b1cf93ef5382fb302f1dd1b
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-27 12:40:14 -0400

    functions_wacky, add prompts for Variables

--- AssociatingJS/functions_Wacky/js/script.js
+++ AssociatingJS/functions_Wacky/js/script.js
@@ -4,15 +4,18 @@
  */
 
 
-alert("I am buying a designer rug and don't any new furniture (sofa or chairs) to touch the new rug. \n\nNot sure of what new furniture can fit with the new rug.");
+//alert("I bought a cool designer rug and don't any new furniture (sofa or chairs) to touch the new rug. \n\nNot sure of what new furniture can fit with the new rug.");
 
-alert("Room is 20X30 ft or 600 square ft.");
+//alert("Room is 20X30 ft or 600 square ft.");
 
-alert("Designer rug is 10X15 ft or 150 sq ft. ");
+//alert("Designer rug is 10X15 ft or 150 sq ft. ");
+
+var sofaLenght =prompt("What is the length of the large sofa?");
+var sofaWidth  =prompt("What is the width of the large sofa?");
 
 var sofa =prompt("Do you want the large or small sofa in the room?");
 
-var totalSofa =largeSofa(10,7);
+var totalSofa =largeSofa(sofaLenght,sofaWidth);
 
 function largeSofa (sofaLenght,sofaWidth){
     var largeSofa = sofaLenght*sofaWidth;

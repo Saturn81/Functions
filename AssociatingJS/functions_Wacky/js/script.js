1dad49444ba1f463585ad3a353a8c0c8c439b3bf
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-27 11:41:38 -0400

    functions_wacky, adding function, and if statements

--- AssociatingJS/functions_Wacky/js/script.js
+++ AssociatingJS/functions_Wacky/js/script.js
@@ -6,3 +6,22 @@
 
 alert("I am buying a designer rug and don't any new furniture (sofa or chairs) to touch the new rug. \n\nNot sure of what new furniture can fit with the new rug.");
 
+alert("Room is 20X30 ft or 600 square ft.");
+
+alert("Designer rug is 10X15 ft or 150 sq ft. ")
+
+var room= 600;
+
+var rug = 150;
+
+function largeSofa (sofaLenght, sofaWidth)
+
+
+var sofa =prompt("Do you want the large or small sofa in the room?");
+
+
+if (sofa=="large"){
+    alert("You can't fit the large sofa in the room");
+}
+
+

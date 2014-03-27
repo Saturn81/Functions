0d5b9e18e4f789d0049e4eddb1e76e33f14756c7
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-27 09:36:34 -0400

    functions_Personal: working on Else If statements

--- AssociatingJS/functions_Personal/js/script.js
+++ AssociatingJS/functions_Personal/js/script.js
@@ -4,7 +4,7 @@
  */
 
 
-alert("I have a lot of leaves to rake up and put in trash bags. \n\nI will pay some neighborhood kids a fixed amount per bag. Have to see how much it will cost me. ")
+alert("I have leaves to rake up and put in trash bags. \n\nI tell neighborhood kids to fill up as many trash bags with leaves, but only 60 bags in container of bags. \n\nWill pay them a fixed amount per bag. Have to see how much it will cost me. ")
 
 
 var lukeBags =prompt("How many trash bags did Luke fill up?")
@@ -13,20 +13,17 @@ var jacobBags =prompt ("How many trash bags did Jacob fill up?")
 
 var moneyPerBag =prompt("How you paying the kids per trash bag?")
 
+var totalSpent = (moneyPerBag*lukeBags)+(moneyPerBag*jacobBags);
 
 
-function lawnMoney (lukeBags,jacobBags,moneyPerBag){
-    var lawnMoney =(Number(lukeBags)+Number(jacobBags))*moneyPerBag;
-    console.log("Hello there this is the answer "+ lawnMoney)
+if (Number(lukeBags)+Number(jacobBags)<=60 && totalSpent<=30){
+    console.log("You can pay Luke $"+(lukeBags*moneyPerBag)+" and Jacob $"+(jacobBags*moneyPerBag)+".");
+}else if(totalSpent>30){
+    console.log("That's too much money per bag, I was thinking of spending no more then $30.00 total for both of them.")
+}else{
+    console.log(("In the original box of trash bags three was only a total of 60 bags, so I only paying for work for 60 trash bags worth."))
 }
 
-lawnMoney (2,3,4);
 
 
-
-console.log(Number(lukeBags)+Number(jacobBags));  //a test prompt
-
-
-
-
-//alert("Test2 for personal ");
+console.log(totalSpent);

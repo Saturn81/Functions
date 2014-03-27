2f9444a098d55a4c7970dd23ce4aafa6ad344bbe
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-27 17:46:03 -0400

    functions_wacky, add another If condition

--- AssociatingJS/functions_Wacky/js/script.js
+++ AssociatingJS/functions_Wacky/js/script.js
@@ -6,22 +6,25 @@
 
 //alert("I bought a cool designer rug and don't any new furniture (sofa or chairs) to touch the new rug. \n\nNot sure of what furniture can fit with the new rug.");
 
-//alert("Room is 10 ft360 square ft.");
+//alert("Room is 10360 square ft.");
 
 //alert("Designer rug is 10X15 ft or 150 sq ft. ");
 
-var sofa =prompt("Do you want the large sofa or chairs in the room?",("large or small"));
+var sofa =prompt("Do you want the large sofa or a few chairs in the room?",("large sofa or chairs"));
 
 if (sofa=="large sofa"||sofa=="Large Sofa"){
     var sofaLenght =prompt("What is the length of the large sofa?");
     var sofaWidth  =prompt("What is the width of the large sofa?");
 }else{
-    var chair = prompt("Chairs are 3X3 ft or 9 square ft, how many chairs you want in the room?")
+    var chair = prompt("Chairs are 12 square ft, how many chairs you want in the room?")
 }
 
+var totalChairs =amountChairs(chair);
 
-
-
+function amountChairs(chair){
+    var amountChairs =chair*12;
+    return amountChairs;
+}
 
 
 var totalLargeSofa =largeSofa(sofaLenght,sofaWidth);
@@ -32,10 +35,11 @@ function largeSofa (sofaLenght,sofaWidth){
 }
 
 if (totalLargeSofa>360){
-    console.log("The sofa is to big for the room, look at a different size sofa.");
+    console.log("The sofa is to big for the room, look at a different size sofa or get some chairs.");
 }else{
     console.log("The sofa fits in the room with the rug.");
 }
 
 
+console.log(totalChairs);
 

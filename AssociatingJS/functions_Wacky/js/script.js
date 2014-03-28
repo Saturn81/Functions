79a64300a24105b5037c0cceb6698b3f444e3ea6
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-28 02:26:22 -0400

    made comments to functions_wacky

--- AssociatingJS/functions_Wacky/js/script.js
+++ AssociatingJS/functions_Wacky/js/script.js
@@ -5,48 +5,47 @@
 
 
 alert("I bought a cool designer rug and don't want any new furniture (sofa or chairs) to touch the new rug. \n\nHave to see what furniture can fit with the new rug.");
+//this describes the formula.
 
+alert("I have 90 sq ft of available space for either furniture combination");  //this alert sets the limit of free space I have to devote to the furniture.
 
-alert("I have 100 sq ft of available space for either furniture combination");
+var sofa =prompt("Do you want the large sofa in the room?",("yes or no"));  //I can not include the sofa and its own fuction if the PROMPT says NO; the other prompts will not be displayed.
 
-var sofa =prompt("Do you want the large sofa in the room?",("yes or no"));
-
-if (sofa=="yes"||sofa=="Yes"){
-    var sofaLenght =prompt("What is the length of the large sofa?");
-    var sofaWidth  =prompt("What is the width of the large sofa?");
+if (sofa=="yes"||sofa=="Yes"){                                                  //If Yes, thren it will give the data for the arguments for the function
+    var sofaLenght =prompt("What is the length of the large sofa? (in Ft.)");   //this is to find the Area of the sofa
+    var sofaWidth  =prompt("What is the width of the large sofa? (in Ft.)");    //this is to find the area of the sofa
 }else{
-    var sofaLenght=0;
-    var sofaWidth=0;
+    var sofaLenght=0;           //I was getting a NAna without this, it took alot of time to figure this one, but I believe it puts into real numbers without the function
+    var sofaWidth=0;            //Same as above.
 }
 
-var chair =prompt("How many chairs do you want in the room?\nChairs are 12 sq. ft",(""));
+var chair =prompt("How many chairs do you want in the room?\nChairs are 12 sq. ft",(""));  //this prompt will give me the paramaters to store the data in for the function.
 
 
-var totalChairs =amountChairs(chair);
+var totalChairs =amountChairs(chair);   //able to return the fuction and re-use it outside mt fuction.
 
-function amountChairs(chair){
-    var amountChairs =chair*12;
-    return amountChairs;
+function amountChairs(chair){         //chair is the parameters
+    var amountChairs =chair*12;         //the 12 is the square ft for each chair,
+    return amountChairs;                //this lets it get pushed into the other parts
+
 }
 
-var totalLargeSofa =largeSofa(sofaLenght,sofaWidth);
+var totalLargeSofa =largeSofa(sofaLenght,sofaWidth);    //able to return the fuction and re-use it outside mt fuction.
 
-function largeSofa (sofaLenght,sofaWidth){
+function largeSofa (sofaLenght,sofaWidth){      //the lenght and width are the paramenters
    var largeSofa = sofaLenght*sofaWidth;
+    console.log("One type of your furniture covers " +amountChairs(chair)+" ft squared" );  //to display the outcome of the function
     return largeSofa;
 }
 
-//var fun =4;
-//var tune=5;
+//console.log("The other type of your furniture covers " +largeSofa(sofaLenght,sofaWidth)+ " ft squared" );
 
 
-
-if (totalChairs+totalLargeSofa>100){
-    console.log("The furniture covers a total of "+(totalChairs+totalLargeSofa)+ "sq ft, which is to big for the room, look at a different size sofa or amount of chairs");
+if (totalChairs+totalLargeSofa>90){      //if the total sq. ft of the two fuctions are above 90, then the statment is true.
+    console.log("The furniture covers a total of "+(totalChairs+totalLargeSofa)+ "sq ft, which is to big for the room, look at a different size sofa or amount of chairs");  //this is displayed if TRUE.
 }else{
-    console.log("You have enough room for your furniture and the brand new rug.");
+    console.log("You have enough room for your furniture and the brand new rug.");      //If False, then the statment is displayed, showing that you have enough room for the furniture. 
 }
 
 
 
-console.log(amountChairs(tune));

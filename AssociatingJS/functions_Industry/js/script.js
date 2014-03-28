2c41beae26c4d98463ceead9b72f94d295f419b4
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-27 22:20:11 -0400

    functions_industry, add function, about to add If statements

--- AssociatingJS/functions_Industry/js/script.js
+++ AssociatingJS/functions_Industry/js/script.js
@@ -5,3 +5,10 @@
 
 var grade = prompt ("What was your grade for the assignment ");
 var points= prompt ("How many points is the assignment worth");
+
+function assignmentGrade (grade, points){
+    var assignmentGrade = (grade/100)*points;
+    console.log(assignmentGrade);
+}
+
+assignmentGrade(grade,points)
